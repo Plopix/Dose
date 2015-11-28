7dbac86397b9bd8d2ff22a36710be372acf18835
Morel Sébastien <morel.seb@gmail.com>
2015-11-28 15:47:59 -0800

    Initial Commit

---
+++ Dose/Dose.h
@@ -0,0 +1,19 @@
+//
+//  Dose.h
+//  Dose
+//
+//  Created by Sebastien Morel on 11/28/15.
+//  Copyright © 2015 Plopix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dose.
+FOUNDATION_EXPORT double DoseVersionNumber;
+
+//! Project version string for Dose.
+FOUNDATION_EXPORT const unsigned char DoseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dose/PublicHeader.h>
+
+
