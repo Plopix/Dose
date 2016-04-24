f5acbaf9f5c1d4777eb1f4c0896626b470f7bcf0
Morel Sébastien <morel.seb@gmail.com>
2016-04-24 12:21:12 -0700

    travis iphonesimulator 9.2

---
+++ Source/Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  Dose
+//
+//  Created by Sebastien Morel on 4/22/16.
+//  Copyright © 2016 Plopix. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Dose"
+)
